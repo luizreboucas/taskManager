dd89a6692e45a678851f9429d70d516e09a6ef7e
Pedro Ribeiro <ribeiro.tpedro@gmail.com>
2024-03-12 21:01:03 -0300

    implementação de login

--- api/controllers/UserController.ts
+++ api/controllers/UserController.ts
@@ -1,27 +1,34 @@
-import { Request, Response } from "express";
-import User from "../models/userModel";
+import { Request, Response } from 'express';
+import User from '../models/userModel';
+import { hash } from 'bcryptjs';
 
 class UserController {
-
-    static getUsers = async(req: Request,res: Response) => {
-        try {
-            const users = await User.find().populate('tasks').exec();
-            res.status(200).json(users)
-        } catch (error) {
-            console.log(error)
-        }
+  static getUsers = async (req: Request, res: Response) => {
+    try {
+      const users = await User.find().populate('tasks').exec();
+      res.status(200).json(users);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    static createUser = async(req: Request, res: Response) => {
-        try {
-            const {nome, email, senha} = req.body
-            const user = new User({nome, email, senha})
-            user.save();
-            res.status(201).json({message: "usuário criado com sucesso", user})
-        } catch (error) {
-            console.log(error)
-        }
+  static createUser = async (req: Request, res: Response) => {
+    try {
+      const { nome, email, senha } = req.body;
+      const hashedPassword = await hash(senha, 8);
+
+      const user = new User({ nome, email, hashedPassword });
+
+      if (await User.findOne({ email: email })) {
+        throw new Error('email já cadastrado');
+      }
+
+      user.save();
+      res.status(201).json({ message: 'usuário criado com sucesso', user });
+    } catch (error) {
+      console.log(error);
     }
+  };
 }
 
-export default UserController
+export default UserController;
--- api/routes/index.ts
+++ api/routes/index.ts
@@ -1,13 +1,15 @@
-import express, {Express} from 'express';
+import express, { Express } from 'express';
 import userRoutes from './userRoutes';
 import taskRoutes from './taskRoutes';
 import cors from 'cors';
+import loginRoutes from './loginRoutes';
 
 const routes = (app: Express) => {
-    app.use(cors())
-    app.use(express.json())
-    app.use(userRoutes)
-    app.use(taskRoutes)
-}
+  app.use(cors());
+  app.use(express.json());
+  app.use(userRoutes);
+  app.use(taskRoutes);
+  app.use(loginRoutes);
+};
 
 export default routes;
