b664b5452c9f41ac840c07902f22ded7ae004d2f
Rhamanda Lima <cathyannarhamanda@gmail.com>
2024-03-15 00:26:40 -0300

    feat: add registration module

--- task-manager-web/src/app/modules/mainframe/mainframe-routing.module.ts
+++ task-manager-web/src/app/modules/mainframe/mainframe-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainframePageComponent } from './pages/mainframe-page/mainframe-page.component';
 import { HomePageComponent } from '../home/pages/home-page/home-page.component';
+import { RegistrationComponent } from '../registration/pages/registration/registration.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
       {
         path: '',
         component: HomePageComponent
+      },
+      {
+        path: 'registration',
+        component: RegistrationComponent
       }
     ]
   }
--- task-manager-web/src/app/shared/shared.module.ts
+++ task-manager-web/src/app/shared/shared.module.ts
@@ -5,10 +5,34 @@ import { HeaderComponent } from './components/header/header.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [FooterComponent, HeaderComponent],
-  imports: [CommonModule, MatToolbarModule, MatIconModule],
-  exports: [HeaderComponent, FooterComponent]
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ]
 })
 export class SharedModule {}
